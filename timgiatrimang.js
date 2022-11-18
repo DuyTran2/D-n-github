e9b25870042e01c44fb28a092f753b08172f066f
DuyTran2 <duy941998@gmail.com>
2022-11-17 22:52:56 -0800

    tim gia tri mang

---
+++ timgiatrimang.js
@@ -0,0 +1,7 @@
+let value = prompt("Enter a number");
+let numbers = [4, 10,12,14,17,20];
+for (let i = 0; i < numbers.length; i++){
+    if ( value == numbers[i]){
+        alert(number[i] + "found at" +i);
+    }
+}
